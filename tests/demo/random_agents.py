ff8563834c2d6c54d8e6ee0352a79d09d46a7b31
pnhung177 <pnhung177@gmail.com>
2020-06-22 21:44:52 +0700

    Add testcases for generate_agents() function

--- tests/demo/random_agents.py
+++ tests/demo/random_agents.py
@@ -3,10 +3,11 @@ import os, sys
 sys.path.insert(0, os.path.dirname(os.path.realpath(__file__)) + '/../../lib')
 
 from pprint import pprint
-
 from modelmock.faker import generate_agents
 
-if True:
+_demo = os.environ.get('MODELMOCK_DEMO_GEN_AGENTS', '1')
+
+if _demo == '1':
   pprint(list(generate_agents(10,
       [
         {'level': 'ND', 'count': 1},
@@ -17,4 +18,11 @@ if True:
         {'level': 'SM', 'min': 2, 'max': 10},
         {'level': 'ST', 'count': 0},
         {'level': 'SA', 'count': 0}
+      ])))
+
+if _demo == '2':
+  pprint(list(generate_agents(7,
+      [
+        {'level': 'ST', 'count': 2},
+        {'level': 'SA', 'count': 3}
       ])))
--- tests/units/faker_test.py
+++ tests/units/faker_test.py
@@ -5,3 +5,18 @@ import os, sys
 
 sys.path.insert(0, os.path.dirname(os.path.realpath(__file__)) + '/../../lib')
 
+from modelmock.faker import generate_agents
+
+
+class generate_agents_test(unittest.TestCase):
+
+  def setUp(self):
+    pass
+
+  def test_ok(self):
+    _agents = list(generate_agents(6, [
+        {'level': 'A', 'count': 1},
+        {'level': 'B', 'count': 2},
+        {'level': 'C'}
+    ]))
+    self.assertEqual(len(_agents), 6)

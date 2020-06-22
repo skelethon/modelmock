2a3ded0f139f8522fd930f47bdaf15ad609ea43c
pnhung177 <pnhung177@gmail.com>
2020-06-22 22:57:40 +0700

    Add testcases for generate_agents() function

--- tests/units/faker_test.py
+++ tests/units/faker_test.py
@@ -13,7 +13,7 @@ class generate_agents_test(unittest.TestCase):
   def setUp(self):
     pass
 
-  def test_ok(self):
+  def test_default_subpath_ok(self):
     _agents = list(generate_agents(6, [
         {'level': 'A', 'count': 1},
         {'level': 'B', 'count': 2},
@@ -26,4 +26,19 @@ class generate_agents_test(unittest.TestCase):
       _counts[_agent['record']['level']] = _counts[_agent['record']['level']] + 1
 
     self.assertEqual(_counts, dict(A=1,B=2,C=3))
+
+
+  def test_without_subpath_ok(self):
+    _agents = list(generate_agents(6, [
+        {'level': 'A', 'count': 2},
+        {'level': 'B', 'count': 4},
+        {'level': 'C'}
+    ], subpath=None))
+    self.assertEqual(len(_agents), 6)
+
+    _counts = dict(A=0,B=0,C=0)
+    for _agent in _agents:
+      _counts[_agent['level']] = _counts[_agent['level']] + 1
+
+    self.assertEqual(_counts, dict(A=2,B=4,C=0))
 

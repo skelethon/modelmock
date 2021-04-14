8ec546407e8d4649f497961a105eaf6dce5e9390
Minh Duc <tongminhduc30061997@gmail.com>
2021-04-14 14:20:49 +0700

    [UNITTEST] add unit tests for PersonInfoInjector class

--- src/modelmock/injectors/__init__.py
+++ src/modelmock/injectors/__init__.py
@@ -2,3 +2,4 @@
 
 from .impl.date_time import DateTimeInjector, DateTimeRandomizer
 from .impl.person_info import PersonInfoInjector
+from .impl.person_info.data.__seeds_vi_VN import personal_lookup

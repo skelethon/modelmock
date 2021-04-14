41e99354cc6ae01829db42bc38028362765cec81
KT <tunglee01230@gmail.com>
2021-04-14 09:19:16 +0700

    [UNITTEST] add the test case for generate_purchases function

--- tests/units/fakers_test.py
+++ tests/units/fakers_test.py
@@ -1,80 +1,80 @@
 #!/usr/bin/env python3
-
+import random
 import unittest
 import os, sys
 
 sys.path.insert(0, os.path.dirname(os.path.realpath(__file__)) + '/../../src')
 
-from modelmock.fakers import generate_agents, generate_contracts
+from modelmock.fakers import generate_agents, generate_contracts, generate_purchases
 
 
 class generate_agents_test(unittest.TestCase):
 
-  def setUp(self):
-    pass
+    def setUp(self):
+        pass
 
-  def test_default_subpath_ok(self):
-    _agents = list(generate_agents(6, [
-        {'level': 'A', 'count': 1},
-        {'level': 'B', 'count': 2},
-        {'level': 'C'}
-    ]))
-    self.assertEqual(len(_agents), 6)
+    def test_default_subpath_ok(self):
+        _agents = list(generate_agents(6, [
+            {'level': 'A', 'count': 1},
+            {'level': 'B', 'count': 2},
+            {'level': 'C'}
+        ]))
+        self.assertEqual(len(_agents), 6)
 
-    _counts = dict(A=0,B=0,C=0)
-    for _agent in _agents:
-      _counts[_agent['record']['level']] = _counts[_agent['record']['level']] + 1
+        _counts = dict(A=0, B=0, C=0)
+        for _agent in _agents:
+            _counts[_agent['record']['level']] = _counts[_agent['record']['level']] + 1
 
-    self.assertEqual(_counts, dict(A=1,B=2,C=3))
+        self.assertEqual(_counts, dict(A=1, B=2, C=3))
+
+    def test_without_subpath_ok(self):
+        _agents = list(generate_agents(6, [
+            {'level': 'A', 'count': 2},
+            {'level': 'B', 'count': 4},
+            {'level': 'C'}
+        ], subpath=None))
+        self.assertEqual(len(_agents), 6)
+
+        _counts = dict(A=0, B=0, C=0)
+        for _agent in _agents:
+            _counts[_agent['level']] = _counts[_agent['level']] + 1
+
+        self.assertEqual(_counts, dict(A=2, B=4, C=0))
 
 
-  def test_without_subpath_ok(self):
-    _agents = list(generate_agents(6, [
-        {'level': 'A', 'count': 2},
-        {'level': 'B', 'count': 4},
-        {'level': 'C'}
-    ], subpath=None))
-    self.assertEqual(len(_agents), 6)
+class generate_purchases_test(unittest.TestCase):
 
-    _counts = dict(A=0,B=0,C=0)
-    for _agent in _agents:
-      _counts[_agent['level']] = _counts[_agent['level']] + 1
+    def setUp(self):
+        pass
 
-    self.assertEqual(_counts, dict(A=2,B=4,C=0))
+    def test_default_ok(self):
+        _record = generate_purchases(10, 50, 2, 100)
+        self.assertEqual(len(list(_record)), 50)
 
-class generate_contracts_test(unittest.TestCase):
+    def test_generate_purchases_with_value_of_totalcontracts_less_than_double_value_of_totalagents(self):
+        errmsgs = 'variance must be greater than 0'
+        check_num = 0
 
-  def setUp(self):
-    pass
+        for i in range(5):
+            check_num += 1
+            with self.assertRaisesRegex(Exception) as context:
+                generate_purchases(10, random.randint(0, 19), 100, 10000000)
+                self.assertEqual(errmsgs, context.exception)
 
-  def test_defaul_ok(self):
-    _records = generate_contracts(6, 100, 1000)
-    self.assertEqual(len(list(_records)), 6)
+        self.assertEqual(check_num, 5)
 
-  def test_value_of_contract_price_less_than_2(self):
-    with self.assertRaises(ValueError):
-      generate_contracts(8, 1, 1000)
-      generate_contracts(8, 0, 1000)
-      generate_contracts(1, 1, 10000)
+    def test_length_of_purchases_ids(self):
+        _records = generate_purchases(10, 20, 50, 100000)
+        check_num = 0
+        check_bool = True
 
-  def test_sum_fyps_equal_multiple_totalagents_and_contract_price(self):
-    _records = generate_contracts(10, 100, 1000)
-    _total = sum([record['fyp'] for record in list(_records)])
-    self.assertEqual(_total, 10*100*1000)
+        for _record in list(_records):
+            check_num += 1
+            if not 12 == len(_record['id']):
+                check_bool = False
 
-  def test_value_type(self):
-    _records = generate_contracts(6, 9, 10000)
-    _check = True
+        self.assertEqual(check_bool, True)
+        self.assertEqual(check_num, 20)
 
-    for record in _records:
-      _fields = list(record)
-      _fields_type = []
 
-      for i in _fields:
-        if 'type' in i:
-          _fields_type.append(i)
 
-      for field in _fields_type:
-        _check = False if record[field] > 3 else True
-
-    self.assertEqual(True,_check)
